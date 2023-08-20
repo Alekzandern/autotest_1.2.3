5eff59fb4bc13ee7573eb93613db5a43c76e6802
Alekzandern <130380585+Alekzandern@users.noreply.github.com>
2023-08-20 16:52:06 +0000

    Add files via upload

---
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -0,0 +1,27 @@
+package ru.netology;
+
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class MobileBankApiTestV1 {
+    @Test
+    void shouldReturnDemoAccounts() {
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Hi")
+
+                .when()
+                .post("/post")
+
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Hey"))
+        ;
+    }
+}
+
+
